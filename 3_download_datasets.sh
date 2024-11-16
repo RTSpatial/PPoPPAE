12529e8c713da6f27faabee219b00764ec4c4844
Liang Geng <pwrliang@gmail.com>
2024-11-15 21:38:27 -0500

    fix download

--- 3_download_datasets.sh
+++ 3_download_datasets.sh
@@ -8,18 +8,22 @@ function download_unzip() {
   name="$2"
   checksum=$3
   filename="$name.tar.bz2"
-  if [[ ! -d "$name" ]]; then
-    conda run -n ppopp-ae-python --live-stream python3 download_dataset.py "$link" \
-      "$filename"
-
-    if [[ $(md5sum "$filename") == "$checksum" ]]; then
+  if [[ ! -f ".$name" ]]; then
+    if [[ ! -f "$filename" ]]; then
+      conda run -n ppopp-ae-python --live-stream python3 "$AE_HOME"/download_dataset.py "$link" \
+        "$filename"
+    fi
+    download_checksum=$(md5sum "$filename" | awk '{ print $1 }')
+    if [[ $download_checksum == "$checksum" ]]; then
+      rm -rf "$name" # remove dataset folder
       tar jxvf "$filename"
     else
-      echo "checksum of $filename is incorrect"
+      echo "checksum of $filename is incorrect, $download_checksum vs $checksum"
       exit 1
     fi
+    touch ".$name"
   else
-    echo "directory $name exists, skip"
+    echo "$name is downloaded, skip"
   fi
 
 }
@@ -33,8 +37,8 @@ if [[ ! -f "$AE_HOME/.datasets" ]]; then
     "polygons" \
     "266d9febc270f0f8e0e78fc49c5eca64"
 
-  download_unzip "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/geng_161_buckeyemail_osu_edu/ER3IFW1VVs9AkzWdrmd4egkBCKv4SPPhb8JuBRvTpbfOCA?e=8cs2XK"
-  "queries" \
+  download_unzip "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/geng_161_buckeyemail_osu_edu/ER3IFW1VVs9AkzWdrmd4egkBCKv4SPPhb8JuBRvTpbfOCA?e=8cs2XK" \
+    "queries" \
     "8117bdc5bee83d2fa37a1558cc733643"
 
   download_unzip "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/geng_161_buckeyemail_osu_edu/EXEkg7cDJMZOrlbZo3oilFAB4m_BmxIVUCW9slplqxYfTA?e=lkpbxo" \
